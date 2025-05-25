bc6964f40757f9c965abeebfc0368945377d571b
Joshua Tite <76907391+ZombiesmasherJT@users.noreply.github.com>
2025-05-25 16:37:16 +0100

    Update Bookings.js

--- src/pages/Bookings.js
+++ src/pages/Bookings.js
@@ -29,15 +29,15 @@ function Bookings() {
         , []);
 
 
-    useEffect(() => {
-        axios.get("https://wanderstay-backend-production.up.railway.app/bookings")
-            .then((response) => {
-                setBookings(response.data);
-            })
-            .catch((error) => {
-                console.log("error: ", error);
-            });
-    }, []);
+  //  useEffect(() => {
+    //    axios.get("https://wanderstay-backend-production.up.railway.app/bookings")
+      //      .then((response) => {
+        //        setBookings(response.data);
+          //  })
+           // .catch((error) => {
+            //    console.log("error: ", error);
+          //  });
+   // }, []);
 
 
     //code to ensure that a user is logged in to be able to do a post booking request
